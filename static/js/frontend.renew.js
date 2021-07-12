89e35ff4d1c058bd6a1e60c3067271c273174182
hyeona1004 <mlmhusdk14@hanmail.net>
2021-07-12 16:12:47 +0900

    7차 공유 - 현재 작업까지 공유

--- static/js/frontend.renew.js
+++ static/js/frontend.renew.js
@@ -8,6 +8,9 @@ renewCommon = (function(doc, win){
         renewCommon.aTab.init();
         renewCommon.rdToggle.init();
         renewCommon.chckToggle.init();
+        renewCommon.infoLineToggle.init();
+        renewCommon.aTooltip.init();
+        renewCommon.toggleBx.init();
     }
 
     //accordion
@@ -95,17 +98,19 @@ renewCommon = (function(doc, win){
                         let _this = this;
                         let _targetName = this.getAttribute('name');
                         let _targetClass = this.getAttribute('data-toggle-target');
-                        let _targetCon = doc.querySelector('.' + _targetClass);
+                        let _targetCon = doc.querySelectorAll('.' + _targetClass);
 
                         renewCommon.rdToggle.resetActive(_targetName);
 
                         if(_targetCon){
-                            let _targetConName = _targetCon.getAttribute('data-name');
-                            if(_this.checked  && _targetName == _targetConName){
-                                _targetCon.classList.remove('is-hidden-strong');
-                            }else{
-                                _targetCon.classList.add('is-hidden-strong');
-                            }
+                            Array.prototype.slice.call(_targetCon).forEach( function (_obj) {
+                                let _targetConName = _obj.getAttribute('data-name');
+                                if(_this.checked  && _targetName == _targetConName){
+                                    _obj.classList.remove('is-hidden-strong');
+                                }else{
+                                    _obj.classList.add('is-hidden-strong');
+                                }
+                            })
                         }
                     });
                 });
@@ -128,6 +133,136 @@ renewCommon = (function(doc, win){
                                 _targetCon.classList.remove('is-hidden-strong');
                             }else{
                                 _targetCon.classList.add('is-hidden-strong');
+                            }
+                        }
+                    });
+                });
+            }
+        }
+    }
+
+    //info-line-toggle
+    obj.infoLineToggle = {
+        changeBtnMode: function(_obj){
+            let tempLi = _obj.closest('li');
+            console.log(tempLi.classList)
+            if(tempLi.classList.contains('mode-edit')){
+                tempLi.classList.remove('mode-edit');
+            }else{
+                tempLi.classList.add('mode-edit');
+            }
+        },
+        init: function(){
+            let toggleTrigger= doc.querySelectorAll('.toggle-info-line-li-btn');
+            if(toggleTrigger) {
+                Array.prototype.slice.call(toggleTrigger).forEach( function (_obj) {
+                    _obj.addEventListener('click', function (_evt) {
+                        _evt.preventDefault();
+                        _evt.stopPropagation();
+                        renewCommon.infoLineToggle.changeBtnMode(this);
+                        let tempUl = this.closest('ul');
+                        let tempLi = tempUl.querySelectorAll('.info-line-toggle-li');
+                        if(tempLi){
+                            Array.prototype.slice.call(tempLi).forEach( function (_obj) {
+                                if(_obj.classList.contains('mode-edit')){
+                                    _obj.classList.remove('mode-edit')
+                                }else {
+                                    _obj.classList.add('mode-edit')
+                                }
+                            })
+                        }
+                    });
+                });
+            }
+        }
+    }
+
+    //aTab
+    obj.aTooltip = {
+        init: function(){
+            let openTooltipBtns = doc.querySelectorAll('.open-tooltip-btn'),
+                aTabCon = doc.querySelectorAll('.a-tab-con');
+            if(openTooltipBtns) {
+                Array.prototype.slice.call(openTooltipBtns).forEach( function (_obj) {
+                    _obj.addEventListener('click', function (_evt) {
+                        _evt.preventDefault();
+                        _evt.stopPropagation();
+                        if (this.parentNode.classList.contains("is-open")) {
+                            this.parentNode.classList.remove("is-open");
+                        }else{
+                            this.parentNode.classList.add("is-open");
+                        }
+                    });
+                });
+            }
+        }
+    }
+
+    //배송지 수정, 삭제 버튼 클릭이벤트
+    obj.deliverListEdit = {
+        init: function(){
+            let btnDlvrEdit = document.querySelectorAll('.delivery-item-edit');
+            let btnDlvrDel = document.querySelectorAll('.delivery-item-del');
+            if(btnDlvrEdit) {
+                Array.prototype.slice.call(btnDlvrEdit).forEach( function (_obj) {
+                    _obj.addEventListener('click', function (_evt) {
+                        _evt.preventDefault();
+                        _evt.stopPropagation();
+                        let tempLi = this.closest('li');
+                        if(tempLi && !tempLi.classList.contains('mode-edit')){
+                            tempLi.classList.add('mode-edit')
+                        }
+                    });
+                });
+            }
+            let btnDlvrEditCancle = document.querySelectorAll('.btn-my-dlvr-cancle');
+            if(btnDlvrEditCancle){
+                Array.prototype.slice.call(btnDlvrEditCancle).forEach( function (_obj) {
+                    _obj.addEventListener('click', function (_evt) {
+                        _evt.preventDefault();
+                        _evt.stopPropagation();
+                        let tempLi = this.closest('li');
+                        if(tempLi && tempLi.classList.contains('mode-edit')){
+                            tempLi.classList.remove('mode-edit')
+                        }
+                    });
+                });
+            }
+        }
+    }
+
+    //toggle-bx
+    obj.toggleBx = {
+        resetBtn: function(_this){
+            let toggleBxBtn = doc.querySelectorAll('.toggle-bx-btn');
+            Array.prototype.slice.call(toggleBxBtn).forEach( function (_obj) {
+                if(_obj != _this){
+                    _obj.classList.remove("active")
+                }
+            });
+        },
+        init: function(){
+            let toggleBxBtn = doc.querySelectorAll('.toggle-bx-btn'),
+                toggleBxCon = doc.querySelectorAll('.toggle-bx-con');
+            if(toggleBxBtn) {
+                Array.prototype.slice.call(toggleBxBtn).forEach( function (_obj) {
+                    _obj.addEventListener('click', function (_evt) {
+                        let _this = this;
+                        let _targetClass = this.getAttribute('data-target');
+                        let _target = doc.querySelector('.toggle-bx-con.' + _targetClass);
+                        if(_target){
+                            if(toggleBxCon) {
+                                renewCommon.toggleBx.resetBtn(_this);
+                                Array.prototype.slice.call(toggleBxCon).forEach(function (_con) {
+                                    _con.classList.remove("active")
+                                });
+                            }
+                            if (this.classList.contains("active")) {
+                                this.classList.remove("active");
+                                _target.classList.remove("active");
+                            } else {
+                                this.classList.add("active");
+                                _target.classList.add("active");
                             }
                         }
                     });
